050c0d03a48130079484e2ae191e7b9459360232
JoelJimenez <jjimenezch@uni.pe>
2022-08-03 06:14:09 -0500

    Identificando los Id de carta y menu

--- Delivery Fast/app/src/main/java/pe/uni/jjimenezch/deliveryfast/Carta.java
+++ Delivery Fast/app/src/main/java/pe/uni/jjimenezch/deliveryfast/Carta.java
@@ -3,12 +3,34 @@ package pe.uni.jjimenezch.deliveryfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class Carta extends AppCompatActivity {
+
+    CheckBox plato7;
+    CheckBox plato8;
+    CheckBox plato9;
+    CheckBox plato10;
+    CheckBox plato11;
+    CheckBox plato12;
+
+    Button pedir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carta);
+
+
+        plato7=findViewById(R.id.plato7);
+        plato8=findViewById(R.id.plato8);
+        plato9=findViewById(R.id.plato9);
+        plato10=findViewById(R.id.plato10);
+        plato11=findViewById(R.id.plato11);
+        plato12=findViewById(R.id.plato12);
+
+        pedir = findViewById(R.id.pedir);
+
     }
 }
--- Delivery Fast/app/src/main/java/pe/uni/jjimenezch/deliveryfast/Menu.java
+++ Delivery Fast/app/src/main/java/pe/uni/jjimenezch/deliveryfast/Menu.java
@@ -3,12 +3,40 @@ package pe.uni.jjimenezch.deliveryfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class Menu extends AppCompatActivity {
+
+
+    CheckBox plato1;
+    CheckBox plato2;
+    CheckBox plato3;
+    CheckBox plato4;
+    CheckBox plato5;
+    CheckBox plato6;
+
+    Button pedir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        plato1=findViewById(R.id.plato1);
+        plato2=findViewById(R.id.plato2);
+        plato3=findViewById(R.id.plato3);
+        plato4=findViewById(R.id.plato4);
+        plato5=findViewById(R.id.plato5);
+        plato6=findViewById(R.id.plato6);
+
+        pedir = findViewById(R.id.pedir);
+
+
+
+
+
+
+
     }
 }
