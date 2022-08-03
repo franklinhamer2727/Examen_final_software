ff9f782eeb8a4ee2582a1e1248cdbf4ec31e2996
Abraham ✨ <65505303+AbrahamBerrospiCasano@users.noreply.github.com>
2022-08-03 15:10:10 -0500

    Update Principal.java

--- src/factoryMethods/Principal.java
+++ src/factoryMethods/Principal.java
@@ -1,23 +1,59 @@
 import  java.io.*;
+import java.util.Objects;
 
-public class Principal {
-    public static void main(String[] args) throws IOException{
+class principal{
+    public static void main(String[] args) throws IOException {
         GetPedido ido = new GetPedido();
 
-        System.out.println("Ingrese el nombre del pedido");
+
+        System.out.println("Seleccione el tipo de plato: ");
+        System.out.println("Carta");
+        System.out.println("Menu");
+        System.out.println("Ingrese sus respuesta:");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
         String compra = br.readLine();
-        System.out.println("Ingrese el numero de pedidos");
+        System.out.println("Cuantos pedidos: ");
         int unidades =Integer.parseInt(br.readLine());
 
         Pedido p = ido.getPedido(compra);
 
-        System.out.print("su Pedido es  "+ido+"unidades : "+unidades);
+        System.out.print("Su cuenta es:");
         p.getPrecio();
-        //p.Calculator();
-    
-    }
+        p.Calculator(unidades);
 
+
+        //Command
+        SuperTarea st= new SuperTarea();
+        st.addTarea(new TareaRegistrarPlato());
+        st.addTarea(new TareaListarPlato());
+        System.out.println("Ingrese su pedido");
+        String pedido = br.readLine();
+
+        GestorTareas gt= new GestorTareas();
+        Plato pla= new Plato(1,pedido);
+
+        gt.ejecutar(st, pla);
+
+        //bridge
+
+        System.out.println("Como Desea cancelar : ");
+        System.out.println("1) Targeta");
+        System.out.println("2) Efectivo");
+        System.out.println("Seleccione una opcion:");
+        String opcion = br.readLine();
+        if (Objects.equals(opcion, "Targeta")){
+            MetodoPago pa = new Tarjeta(new Consumo());
+            pa.pagar();
+            System.out.println("Gracias por su visita!!!!");
+
+        }
+        else{
+            MetodoPago pa = new Efectivo(new Consumo());
+            pa.pagar();
+            System.out.println("   Gracias por su visita!!!!");
+        }
+
+    }
 }
